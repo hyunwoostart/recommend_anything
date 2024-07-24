84e701729fc35ea31893cdc5b9e650c1179140b4
hwji <knjhw@daum.net>
2024-07-24 11:39:46 +0900

    링크 수정

--- src/components/PrdDiscount.js
+++ src/components/PrdDiscount.js
@@ -8,7 +8,6 @@ function PrdDiscount() {
     <>
         <ul className='discount_container'>
             <li>
-
                 <Link to="/product">
                     <div>
                         <div className='img_wrap'>
@@ -27,52 +26,94 @@ function PrdDiscount() {
                 </Link>
             </li>
             <li>
-                <div>
-                    <div className='img_wrap'>
-                        <img alt="img" src={discountImg}/>
-                        <p className='label_discount'>
-                            <span className='percent_num'>10</span>
-                            <span className='percent_wrap'>
-                                <span>%</span>
-                                <span>Off</span>
-                            </span>
-                        </p>
+                <Link to="/product">
+                    <div>
+                        <div className='img_wrap'>
+                            <img alt="img" src={discountImg}/>
+                            <p className='label_discount'>
+                                <span className='percent_num'>15</span>
+                                <span className='percent_wrap'>
+                                    <span>%</span>
+                                    <span>Off</span>
+                                </span>
+                            </p>
+                        </div>
+                        <p>Greys Vage</p>
+                        <span className='label_primary'>6 Days Remaining</span>
                     </div>
-                    <p>Greys Vage</p>
-                    <span className='label_primary'>6 Days Remaining</span>
-                </div>
+                </Link>
             </li>
             <li>
-                <div>
-                    <div className='img_wrap'>
-                        <img alt="img" src={discountImg}/>
-                        <p className='label_discount'>
-                            <span className='percent_num'>25</span>
-                            <span className='percent_wrap'>
-                                <span>%</span>
-                                <span>Off</span>
-                            </span>
-                        </p>
+                <Link to="/product">
+                    <div>
+                        <div className='img_wrap'>
+                            <img alt="img" src={discountImg}/>
+                            <p className='label_discount'>
+                                <span className='percent_num'>15</span>
+                                <span className='percent_wrap'>
+                                    <span>%</span>
+                                    <span>Off</span>
+                                </span>
+                            </p>
+                        </div>
+                        <p>Greys Vage</p>
+                        <span className='label_primary'>6 Days Remaining</span>
                     </div>
-                    <p>Greys Vage</p>
-                    <span className='label_primary'>6 Days Remaining</span>
-                </div>
+                </Link>
             </li>
             <li>
-                <div>
-                    <div className='img_wrap'>  
-                        <img alt="img" src={discountImg}/>
-                        <p className='label_discount'>
-                            <span className='percent_num'>20</span>
-                            <span className='percent_wrap'>
-                                <span>%</span>
-                                <span>Off</span>
-                            </span>
-                        </p>
+                <Link to="/product">
+                    <div>
+                        <div className='img_wrap'>
+                            <img alt="img" src={discountImg}/>
+                            <p className='label_discount'>
+                                <span className='percent_num'>15</span>
+                                <span className='percent_wrap'>
+                                    <span>%</span>
+                                    <span>Off</span>
+                                </span>
+                            </p>
+                        </div>
+                        <p>Greys Vage</p>
+                        <span className='label_primary'>6 Days Remaining</span>
                     </div>
-                    <p>Greys Vage</p>
-                    <span className='label_primary'>6 Days Remaining</span>
-                </div>
+                </Link>
+            </li>
+            <li>
+                <Link to="/product">
+                    <div>
+                        <div className='img_wrap'>
+                            <img alt="img" src={discountImg}/>
+                            <p className='label_discount'>
+                                <span className='percent_num'>15</span>
+                                <span className='percent_wrap'>
+                                    <span>%</span>
+                                    <span>Off</span>
+                                </span>
+                            </p>
+                        </div>
+                        <p>Greys Vage</p>
+                        <span className='label_primary'>6 Days Remaining</span>
+                    </div>
+                </Link>
+            </li>
+            <li>
+                <Link to="/product">
+                    <div>
+                        <div className='img_wrap'>
+                            <img alt="img" src={discountImg}/>
+                            <p className='label_discount'>
+                                <span className='percent_num'>15</span>
+                                <span className='percent_wrap'>
+                                    <span>%</span>
+                                    <span>Off</span>
+                                </span>
+                            </p>
+                        </div>
+                        <p>Greys Vage</p>
+                        <span className='label_primary'>6 Days Remaining</span>
+                    </div>
+                </Link>
             </li>
         </ul>
     </>
